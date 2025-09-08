5848f081a4455ec9465188385bb91690dd73cc19
Harianto 🤖 <hariantoatwork@gmail.com>
2025-09-08 17:48:48 +0200

    fix build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-npx @better-auth/cli generate -y --config ./modules/0000-auth/lib/auth.server.js
-npx @better-auth/cli migrate -y --config ./modules/0000-auth/lib/auth.server.js
+# npx @better-auth/cli generate -y --config ./modules/0000-auth/lib/auth.server.js
+# npx @better-auth/cli migrate -y --config ./modules/0000-auth/lib/auth.server.js
 
-pnpm run db:migrate
+# pnpm run db:migrate
+
+bun ci
